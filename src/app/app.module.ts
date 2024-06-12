f0d0be1408dc44ba5a5bec9c3d39a579ca4f2544
cristianhar <yohan4752@gmail.com>
2024-06-12 13:18:51 -0500

    Coreccion errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { TiendaComponent } from './tienda/tienda.component';
-import { CarritoComponent } from './carrito/carrito.component';
-import { LoginComponent } from './login/login.component';
-import { RegistroComponent } from './registro/registro.component';
+import { HomeComponent } from './Components/home/home.component';
+import { TiendaComponent } from './Components/tienda/tienda.component';
+import { CarritoComponent } from './Components/carrito/carrito.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegistroComponent } from './Components/registro/registro.component';
 
 @NgModule({
   declarations: [
