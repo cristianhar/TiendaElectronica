05b5a90b31326da8561992111cfd4cbb29ec633a
cristianhar <yohan4752@gmail.com>
2024-06-16 19:34:51 -0500

    Mejorando los alert

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +12,19 @@ export class LoginComponent {
   username = '';
   password = '';
 
-  constructor(private serviciodeautenticacion: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private modalService: NgbModal
+  ) { }
 
   logearse(): void {
-    this.serviciodeautenticacion.validarlogeo(this.username, this.password).subscribe(success => {
+    this.authService.validarlogeo(this.username, this.password).subscribe(success => {
       if (success) {
         this.router.navigate(['/home']);
+        this.mostrarModalLoginExitoso();
       } else {
-        alert('Login fallido');
+        this.mostrarModalLoginFallido();
       }
     });
   }
@@ -26,4 +32,34 @@ export class LoginComponent {
   formularioRegistro(): void {
     this.router.navigate(['/registro']);
   }
+
+  // Función para abrir el modal de inicio de sesión exitoso
+  mostrarModalLoginExitoso(): void {
+    this.modalService.open(ModalLoginExitoso);
+  }
+
+  // Función para abrir el modal de inicio de sesión fallido
+  mostrarModalLoginFallido(): void {
+    this.modalService.open(ModalLoginFallido);
+  }
 }
+
+// Componente modal para el inicio de sesión exitoso
+@Component({
+  template: `
+    <div class="modal-body">
+      <p>¡Inicio de sesión exitoso!</p>
+    </div>
+  `
+})
+export class ModalLoginExitoso { }
+
+// Componente modal para el inicio de sesión fallido
+@Component({
+  template: `
+    <div class="modal-body">
+      <p>Inicio de sesión fallido. Verifica tus credenciales e intenta nuevamente.</p>
+    </div>
+  `
+})
+export class ModalLoginFallido { }
